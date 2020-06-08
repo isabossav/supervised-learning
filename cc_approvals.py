d4564f10c1ca8fe255a1134d60f79d57370b24c8
Isabella Bossa <44573636+isabossav@users.noreply.github.com>
2020-06-08 12:03:22 -0400

    Add files via upload

---
+++ cc_approvals.py
@@ -0,0 +1,119 @@
+# Use logistic regression to predict if a credit card application will be approved
+# Project taken from DataCamp
+# Data taken from http://archive.ics.uci.edu/ml/datasets/credit+approval
+# Achieve a best score of 85.07%
+
+# Import necessary modules
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+
+
+# Load dataset
+cc_apps = pd.read_csv("cc_approvals.data", header=None)
+
+# Get summary statistics and information
+cc_apps_description = cc_apps.describe()
+cc_apps_info = cc_apps.info()
+
+
+# Handle missing data 
+# Inspect missing values in the dataset
+# print(cc_apps.tail(17))
+
+# Replace the '?'s (missing values) with NaN
+cc_apps = cc_apps.replace('?', np.nan)
+
+# Impute the missing values with mean imputation
+cc_apps.fillna(cc_apps.mean(), inplace=True)
+
+# Count the number of NaNs in the dataset to verify
+cc_apps.isnull().sum()
+
+# Iterate over each column of cc_apps
+for col in cc_apps.columns:
+    # Check if the column is of object type
+    if cc_apps[col].dtypes == 'object':
+        # Impute with the most frequent value
+        cc_apps = cc_apps.fillna(cc_apps[col].value_counts().index[0])
+
+# Count the number of NaNs in the dataset
+cc_apps.isnull().sum()
+
+
+# Use label encoding
+# Instantiate LabelEncoder
+le = LabelEncoder()
+
+# Iterate over all the values of each column and extract their dtypes
+for col in cc_apps.columns:
+    # Compare if the dtype is object
+    if cc_apps[col].dtypes == 'object':
+    # Use LabelEncoder to do the numeric transformation
+        cc_apps[col]=le.fit_transform(cc_apps[col])
+
+
+# Split data
+# Drop the features 11 and 13 and convert the DataFrame to a NumPy array
+cc_apps = cc_apps.drop([11, 13], axis=1)
+cc_apps = cc_apps.values
+
+# Segregate features and labels into separate variables
+X,y = cc_apps[:,0:13] , cc_apps[:,13]
+
+# Split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                y,
+                                test_size=0.3,
+                                random_state=42)
+
+
+# Scale data
+# Instantiate MinMaxScaler and use it to rescale X_train and X_test
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaledX_train = scaler.fit_transform(X_train)
+rescaledX_test = scaler.fit_transform(X_test)
+
+
+# Logistic Regression
+# Instantiate a LogisticRegression classifier with default parameter values
+logreg = LogisticRegression()
+
+# Fit logreg to the train set
+logreg.fit(rescaledX_train, y_train)
+
+# Use logreg to predict instances from the test set and store it
+y_pred = logreg.predict(rescaledX_test)
+
+# Get the accuracy score of logreg model and print it
+print("Accuracy of logistic regression classifier: ", logreg.score(rescaledX_test, y_test))
+
+# Print the confusion matrix of the logreg model
+print(confusion_matrix(y_test, y_pred))
+
+
+# Fine tune the model using Grid Search
+# Define the grid of values for tol and max_iter
+tol = [0.01, 0.001, 0.0001]
+max_iter = [100, 150, 200]
+
+# Create a dictionary where tol and max_iter are keys and the lists of their values are corresponding values
+param_grid = dict(zip(['tol', 'max_iter'], [tol, max_iter]))
+
+# Instantiate GridSearchCV with the required parameters
+grid_model = GridSearchCV(estimator=logreg, param_grid=param_grid, cv=5)
+
+# Use scaler to rescale X and assign it to rescaledX
+rescaledX = scaler.fit_transform(X)
+
+# Fit data to grid_model
+grid_model_result = grid_model.fit(rescaledX, y)
+
+# Summarize results
+best_score, best_params = grid_model_result.best_score_, grid_model_result.best_params_
+print("Best: %f using %s" % (best_score, best_params))
